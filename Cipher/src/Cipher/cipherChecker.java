f61c5d33f4dd6b2acb37cbcfed54a796b61ab5ad
ChristianNater <46952496+ChristianNater@users.noreply.github.com>
2021-02-10 10:43:50 -0500

    Add files via upload

---
+++ Cipher/src/Cipher/cipherChecker.java
@@ -0,0 +1,48 @@
+package Cipher;
+//Base UI
+
+//import javax.swing.*;
+//import java.awt.*;
+//import package.encryption;
+
+public class cipherChecker
+{
+	public static encryption e = new encryption();
+	public static decryption d = new decryption();
+
+	/**
+	 * 
+	 */
+	//private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	/*String test = "Th!s !s a test";
+	String tst = "?ÔW?íò?Æ???÷??";
+	String tst2 = "ùl6ÍÑ÷¦à÷¤?Îã";	
+	
+		int index = 0;
+		char t = e.getCharfromString(tst2, index);
+		int spc = t;
+		//System.out.println(spc);
+		//System.out.print(test.length());
+		e.encrypting(test);
+		//       d.decrypting(tst, tst2);
+		//int punct2 = Integer.parseInt(tst2);
+		//System.out.println(punct2);
+
+		*/
+		
+		
+		
+		
+		
+		
+
+
+
+
+	}
+
+}
