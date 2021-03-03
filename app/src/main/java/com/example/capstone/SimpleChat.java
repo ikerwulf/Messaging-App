781a23b5895b87fcb836ea4b221b28ef4ca16949
ChristianNater <christian.nater@saintleo.edu>
2021-03-03 02:06:03 -0500

    Chat updated; Now displays a single message.

--- app/src/main/java/com/example/capstone/SimpleChat.java
+++ app/src/main/java/com/example/capstone/SimpleChat.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.capstone.ui.main.SimpleChatFragment;
 
@@ -33,11 +34,20 @@ public class SimpleChat extends AppCompatActivity {
 
     public void sendMessage(View view) //Send button response
     {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        //Intent intent = new Intent(this, DisplayMessageActivity.class);
+        Intent intent = getIntent();
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
         intent.putExtra(Xtra_Msg, message);
-        startActivity(intent);
+
+
+        message = intent.getStringExtra(SimpleChat.Xtra_Msg);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView message_output = findViewById(R.id.message_output);
+        message_output.setText(message);
+
+        //startActivity(intent);
     }
 
 
