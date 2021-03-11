b7b874a96523e1a72530a3b7f2428e89d5b7fadc
ChristianS <christiansilva0304@gmail.com>
2021-03-10 21:04:59 -0500

    Chat Interaction Second Implement

--- app/src/main/java/com/example/capstone/SettingsActivity.java
+++ app/src/main/java/com/example/capstone/SettingsActivity.java
@@ -1,17 +1,25 @@
 package com.example.capstone;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreference;
+
 
 public class SettingsActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
         if (savedInstanceState == null) {
@@ -25,7 +33,47 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+
+        SharedPreferences appSettingPrefs = getSharedPreferences("AppSettingPrefs", 0);
+        SharedPreferences.Editor sharedPrefsEdit = appSettingPrefs.edit();
+        boolean isNightModeOn = appSettingPrefs.getBoolean("NightMode", false);
+
+        if(isNightModeOn){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        }
+
+        SwitchPreference switch_btnK;
+
+        Button switch_btn = findViewById(R.id.switch_btn);
+        switch_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isNightModeOn){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    sharedPrefsEdit.putBoolean("NightMode", false);
+                    sharedPrefsEdit.apply();
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    sharedPrefsEdit.putBoolean("NightMode", true);
+                    sharedPrefsEdit.apply();
+
+                }
+            }
+        });
+
+
+
+
+
+
+
     }
+
 
     public void OpenSimpleChat(View view) {
     }
@@ -39,6 +87,9 @@ public class SettingsActivity extends AppCompatActivity {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
         }
+
     }
+
 }
