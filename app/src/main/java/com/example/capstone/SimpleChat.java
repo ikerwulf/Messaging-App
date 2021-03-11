c922c7852fff8863d7b74f43993fa4ccf6a60971
ChristianNater <christian.nater@saintleo.edu>
2021-03-10 21:12:08 -0500

    Chat updated; Now displays a single message.

--- app/src/main/java/com/example/capstone/SimpleChat.java
+++ app/src/main/java/com/example/capstone/SimpleChat.java
@@ -2,6 +2,15 @@ package com.example.capstone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.text.Layout;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.LinearLayout;
+import android.widget.Button;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +26,7 @@ public class SimpleChat extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.simple_chat_activity);
+        //final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linear_layout);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, SimpleChatFragment.newInstance())
@@ -34,20 +44,42 @@ public class SimpleChat extends AppCompatActivity {
 
     public void sendMessage(View view) //Send button response
     {
-        //Intent intent = new Intent(this, DisplayMessageActivity.class);
         Intent intent = getIntent();
+
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
         intent.putExtra(Xtra_Msg, message);
 
 
         message = intent.getStringExtra(SimpleChat.Xtra_Msg);
+        TextView message_out = findViewById(R.id.message_output);
+        RelativeLayout.LayoutParams params1 =  (RelativeLayout.LayoutParams) message_out.getLayoutParams();
+        if(message_out.hasSelection() == false)
+        {
+            // Capture the layout's TextView and set the string as its tex
+            message_out.setText(message);
 
-        // Capture the layout's TextView and set the string as its text
-        TextView message_output = findViewById(R.id.message_output);
-        message_output.setText(message);
+        }
+        else
+        {
+            message_out.getLayoutParams();
+            params1.addRule(RelativeLayout.ABOVE, message_out.getId());
+            message_out.setLayoutParams(params1);
+            TextView nwTxt = findViewById(R.id.message_output);
+            nwTxt.setText(message);
+        }
 
-        //startActivity(intent);
+
+
+        //Creates a new text box
+        //TextView nwTxt = new TextView(SimpleChat.this);
+        //nwTxt.setId(R.id.message_output);
+        //FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.CENTER);
+
+        //((FrameLayout) findViewById(R.id.simple_chat)).addView(nwTxt, params1);
+
+
+
     }
 
 
